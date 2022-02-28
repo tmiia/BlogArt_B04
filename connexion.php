c472d2de21d654adf25138a7abc13289a040882f
Olivia Simonet <olivia.simonet@mmibordeaux.com>
2022-02-28 16:37:02 +0100

    Connexion terminée

--- connexion.php
+++ connexion.php
@@ -5,31 +5,41 @@ require_once __DIR__ . '/back.php';
 require_once __DIR__ . '/CLASS_CRUD/membre.class.php';
 $monMembre = new MEMBRE();
 
-$membre = 'pseudoMemb';
-$motDePasse = 'passMemb';
-
 // INSCRIPTION : champ username / mdp
 // qd utilisateur tape mdp > haché (nouvelle chaîne de chara unique)
 // fonction php pour ça :
 
-$passCrypt = password_hash($motDePasse, PASSWORD_DEFAULT);
+
 
 // Gestion du $_SERVER["REQUEST_METHOD"] => En POST
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
+    $membre = $_POST['eMailMemb'];
+
+    $pseudo = $monMembre->get_1MembreByEmail($membre)['pseudoMemb'];
+
+    $motDePasse = $_POST['passMemb'];
+    $passCrypt = password_hash($motDePasse, PASSWORD_DEFAULT);
+
+    $monMembre->get_1MembreByEmail($_POST['eMailMemb']);
+
+
+
     // fonction qui vérifie que l'utilisateur a bien tapé ce qui correspond au hash de la BDD
     password_verify($_POST['passMemb'], $passCrypt); // retourne un booleen
 
-    connect_user($_POST['pseudoMemb'], $_POST['passMemb']);
+    connect_user($_POST['eMailMemb'], $_POST['passMemb']);
 
     if (password_verify($_POST['passMemb'], $passCrypt) === true) {
-        setcookie('pseudoMemb', $membre, time() + 3600);
+        setcookie('eMailMemb', $membre, time() + 3600);
+        setcookie('pseudoMemb', $pseudo, time() + 3600);
+
     } else {
         echo('Mauvais mdp sorry');
 
     }
-    if(isset($_COOKIE['membre'])) {
-        echo('bonjour' . $_COOKIE['membre'] . '<br>');
+    if(isset($_COOKIE['eMailMemb'])) {
+        echo('bonjour' . $_COOKIE['pseudoMemb'] . '<br>');
     } else {
         echo('Merci de vous connecter.');
     }
@@ -47,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <h2>Connexion</h2>
 
         <label>Pseudo</label>
-        <input type="text" name="pseudoMemb" value=""/><br>
+        <input type="text" name="eMailMemb" value=""/><br>
 
         <label>Mot de passe</label>
         <input type="text" name="passMemb" value=""/><br>
