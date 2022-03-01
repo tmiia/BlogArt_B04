1b340d858f371c6f13a97d42b6298932e6d4a7c1
AylineT <ayline.travers@gmail.com>
2022-03-01 14:33:25 +0100

    modif header

--- front/includes/commons/headerFront.php
+++ front/includes/commons/headerFront.php
@@ -4,17 +4,20 @@
     
     <nav>
         <a href="home.php"><img src="../front/assets/logoAuBordDesRues.png" alt="logoAuBordDesRues"></a>
-        <div>
+        <div class="langues">
             <h3>Fr</h3>
         </div>
 
         <a href="../front/pageArticles.php"><h3>Articles</h3></a>
-        <a href="#"></a><h3>Newsletter</h3></a>
-        <a href="#"></a><h3>Contact</h3></a>
+        <a href="#"><h3>Newsletter</h3></a>
+        <a href="#"><h3>Contact</h3></a>
     </nav>
 
     <div class="connect">
-            <a href="../connexion.php"><h3>Connexion</h3></a>
-            <a href="../inscription.php">Inscription</a>
+        <a href="../connexion.php"><h3>Connexion</h3></a>
+        <div class="btn_inscription">
+            <a href="../inscription.php"><h3>Inscription</h3></a>
+        </div>
+            
     </div>
 </header>
