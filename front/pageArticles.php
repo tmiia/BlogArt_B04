b7a3a20eb7649ccb2cda75b5c256ff7416df87c1
lyam-roux <lyamroux2@gmail.com>
2022-03-01 17:44:17 +0100

    l

--- front/pageArticles.php
+++ front/pageArticles.php
@@ -25,8 +25,7 @@
 
         <h6>THEMATIQUES :</h6>
         <a href="" class="event_butt">
-            <span class="butt">EVENEMENTS<svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"/></svg>
-</span>
+            <span class="butt">EVENEMENTS<img class="svg" src="../front/assets/down.jpg" alt=""></span>
         </a>
 
     </div>
@@ -36,8 +35,7 @@
 
         <h6>TRIER PAR :</h6>
         <a href="" class="date_butt">
-        <span class="butt">PAR DATE<svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"/></svg>
-</span>
+        <span class="butt">PAR DATE<img class="svg" src="../front/assets/down.jpg" alt=""></span>
 
         </a>
 
