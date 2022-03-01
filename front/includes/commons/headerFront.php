0e8a40f05394218447b2c53d45b85db9d563632e
AylineT <ayline.travers@gmail.com>
2022-03-01 14:54:34 +0100

    modif header

--- front/includes/commons/headerFront.php
+++ front/includes/commons/headerFront.php
@@ -5,7 +5,9 @@
     <nav>
         <a href="home.php"><img src="../front/assets/logoAuBordDesRues.png" alt="logoAuBordDesRues"></a>
         <div class="langues">
-            <h3>Fr</h3>
+            <a href="#">
+                <h3>Fr</h3>
+        </a>
         </div>
 
         <a href="../front/pageArticles.php"><h3>Articles</h3></a>
--- front/includes/commons/imports.php
+++ front/includes/commons/imports.php
@@ -1,1 +1,2 @@
 <link rel="stylesheet" href="https://use.typekit.net/bsk4wen.css"> 
+<link rel="stylesheet" type="text/css" href="//use.fontawesome.com/releases/v5.7.2/css/all.css">
