31fafeb8f7630d34b8e62431bc09d494bc98885b
lyam-roux <lyamroux2@gmail.com>
2022-03-01 11:47:34 +0100

    create navigation

--- front/pageArticles.php
+++ front/pageArticles.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="sass/pageArticle.scss" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    
     
 </head>
 
@@ -16,7 +17,7 @@
 <!------------>
 <div class="tri_article">
     
-    <div>
+    <div class="thematique_event">
 
         <h6>THEMATIQUES :</h6>
         <a href="" class="event_butt">EVENEMENTS</a>
