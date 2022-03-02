c550c1501bf251859b705f60b86aca3ba14d4acd
lyam-roux <lyamroux2@gmail.com>
2022-03-02 15:30:15 +0100

    x

---
+++ front/CGU.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nous contacter</title>
+</head>
+<body>
+
+<?php
+    require_once __DIR__ . '/includes/commons/headerFront.php';
+    // require_once __DIR__ . '/includes/commons/navigationFront.php';
+    require_once __DIR__ . '/includes/commons/imports.php';
+?>
+
+<!------------>
+
+  <H1>Conditions générales d'utilisation</H1>  
+
+  <H2>En vigueur au 18/02/2022</H2>
+
+  <p>Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique
+des modalités de mise à disposition du site et des services par _______________ et de définir les
+conditions d’accès et d’utilisation des services par « l'Utilisateur ».
+Les présentes CGU sont accessibles sur le site à la rubrique «CGU».</p>
+
+<br>
+
+<p>Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des
+présentes CGU par l’utilisateur. Lors de l'inscription sur le site via le Formulaire d’inscription, chaque
+utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant : «
+Je reconnais avoir lu et compris les CGU et je les accepte ».
+En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer
+à l'accès des services proposés par le site.
+www.AuBordDesRues.fr se réserve le droit de modifier unilatéralement et à tout moment le contenu
+des présentes CGU.</p>
+
+
+
+
+
+<!------------>
+
+
+<?php
+    require_once __DIR__ . '/includes/commons/footerFront.php';
+    ?>
+
+
+
+</body>
+</html>
