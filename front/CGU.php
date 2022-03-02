83ce279aea97913b3cc962a00bc95398848b8e99
lyam-roux <lyamroux2@gmail.com>
2022-03-02 16:01:05 +0100

    root tu me casse les couilles putain

--- front/CGU.php
+++ front/CGU.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nous contacter</title>
+    <title>CGU</title>
 </head>
 <body>
 
@@ -21,7 +21,7 @@
   <H2>En vigueur au 18/02/2022</H2>
 
   <p>Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique
-des modalités de mise à disposition du site et des services par _______________ et de définir les
+des modalités de mise à disposition du site et des services par Nébulab Agency et de définir les
 conditions d’accès et d’utilisation des services par « l'Utilisateur ».
 Les présentes CGU sont accessibles sur le site à la rubrique «CGU».</p>
 
@@ -35,6 +35,156 @@ En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisa
 à l'accès des services proposés par le site.
 www.AuBordDesRues.fr se réserve le droit de modifier unilatéralement et à tout moment le contenu
 des présentes CGU.</p>
+
+<H3>ARTICLE 1 : Les mentions légales</H3>
+
+                <p class="art">L’édition et la direction de la publication du site www.AuBordDesRues.fr est assurée par Nébula
+                agency, domicilié 1 Rue Jacques Ellul.
+                Numéro de téléphone est 07 ** ** ** **
+                Adresse e-mail Nébula.agency@gmail.com.</p>
+
+                <br>
+
+                <p class="art">L'hébergeur du site www.AuBordDesRues.fr est la société Iut bordeaux montaigne, dont le siège
+                social est situé au 1 Rue Jacques Ellul, avec le numéro de téléphone : 07 ** ** ** **.</p>
+
+<H3>ARTICLE 2 : Accès au site</H3>
+
+                <p>Le site www.AuBordDesRues.fr permet à l'Utilisateur un accès gratuit aux services suivants :
+                Le site internet propose les services suivants :
+                Un blog, le sujet principale est la solidarité
+                Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les
+                frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion
+                Internet, etc.) sont à sa charge.
+                </p>
+
+                <br>
+
+                <p>L’Utilisateur non membre n'a pas accès aux services réservés. Pour cela, il doit s’inscrire en
+                remplissant le formulaire. En acceptant de s’inscrire aux services réservés, l’Utilisateur membre
+                s’engage à fournir des informations sincères et exactes concernant son état civil et ses coordonnées,
+                notamment son adresse email.</p>
+
+                <br>
+
+                <p>Pour accéder aux services, l’Utilisateur doit ensuite s'identifier à l'aide de son identifiant et de son mot
+                de passe qui lui seront communiqués après son inscription.
+                Tout Utilisateur membre régulièrement inscrit pourra également solliciter sa désinscription en se
+                rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable.
+                Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site
+                ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas
+                la responsabilité de www.AuBordDesRues.fr. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir
+                rigueur à l’éditeur de toute interruption ou suspension de service, même sans préavis.
+                L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de
+                l’éditeur communiqué à l’ARTICLE 1.</p>
+
+<H3>ARTICLE 3 : Collecte des données</H3>
+
+                    <p>Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la
+                mesure où il ne collecte aucune donnée concernant les Utilisateurs.
+                </p>
+
+<H3>ARTICLE 4 : Propriété intellectuelle</H3>
+
+                <p>Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet
+d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.</p>
+
+                <br>
+
+                                <p>L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des
+                différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé,
+                toute utilisation à des fins commerciales et publicitaires est strictement interdite.
+                Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation
+                expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2
+                et suivants du Code de la propriété intellectuelle.
+                Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui
+                reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.
+                </p>
+
+<H3>ARTICLE 5 : Responsabilité</H3>
+
+                                <p>Les sources des informations diffusées sur le site www.AuBordDesRues.fr sont réputées fiables mais
+                le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.
+                Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle.
+                Malgré des mises à jour régulières, le site www.AuBordDesRues.fr ne peut être tenu responsable de
+                la modification des dispositions administratives et juridiques survenant après la publication. De même,
+                le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue
+                dans ce site.
+                L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle
+                que soit sa forme, est interdite. Il assume les risques liés à l'utilisation de son identifiant et mot de
+                passe. Le site décline toute responsabilité</p>
+
+                <br>
+                
+                                <p>Le site www.AuBordDesRues.fr ne peut être tenu pour responsable d’éventuels virus qui pourraient
+                infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou
+                au téléchargement provenant de ce site.
+                La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et
+                insurmontable d'un tiers.</p>
+
+<H3>ARTICLE 6 : Liens hypertextes</H3>
+
+                <p>Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces
+liens, il sortira du site www.AuBordDesRues.fr. Ce dernier n’a pas de contrôle sur les pages web sur
+lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.
+</p>
+
+<h3>ARTICLE 7 : Cookies
+</h3>
+
+                <p>L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement
+sur son logiciel de navigation.
+Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de
+l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site www.AuBordDesRues.fr.
+Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier
+quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains
+cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.
+L’information contenue dans les cookies est utilisée pour améliorer le site www.AuBordDesRues.fr.
+En naviguant sur le site, L’Utilisateur les accepte.
+L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son
+logiciel de navigation.</p>
+
+<H3>ARTICLE 8 : Publication par l’Utilisateur</H3>
+
+<p>Le site permet aux membres de publier les contenus suivants :
+Des commentaire au article du blog.
+Dans ses publications, le membre s’engage à respecter les règles de la Netiquette (règles de bonne
+conduite de l’internet) et les règles de droit en vigueur.
+Le site peut exercer une modération sur les publications et se réserve le droit de refuser leur mise en
+ligne, sans avoir à s’en justifier auprès du membre.
+Le membre reste titulaire de l’intégralité de ses droits de propriété intellectuelle. Mais en publiant une
+publication sur le site, il cède à la société éditrice le droit non exclusif et gratuit de représenter,
+reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé,
+dans le monde entier, sur tout support (numérique ou physique), pour la durée de la propriété
+intellectuelle. Le Membre cède notamment le droit d'utiliser sa publication sur internet et sur les
+réseaux de téléphonie mobile.
+La société éditrice s'engage à faire figurer le nom du membre à proximité de chaque utilisation de sa
+publication.
+</p>
+
+<br>
+
+<p>Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne
+pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout
+recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur.
+Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié
+par le site, sans préavis</p>
+
+
+<H3>ARTICLE 9 : Droit applicable et juridiction compétente</H3>
+
+<p class="end">La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un
+litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.
+Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux
+coordonnées inscrites à l’ARTICLE 1.
+</p>
+
+<br>
+
+
+class="end"
+
+
 
 
 
