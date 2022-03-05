f7874f52ca186611f5cd53be6a9f2ef46b9ba9d1
Olivia Simonet <olivia.simonet@mmibordeaux.com>
2022-03-05 16:33:59 +0100

    Corrections mentions légales

--- front/mentionlegal.php
+++ front/mentionlegal.php
@@ -21,52 +21,60 @@
 <!------------>
 <div class="cgu">
 
-<h1>Mentions légales</h1>
-<h2>En vigueur au 18/02/2022</h2>
+    <h1>Mentions légales</h1>
+    <h2>En vigueur au 18/02/2022</h2>
 
-<p>Conformément aux dispositions des articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la
-Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et
-visiteurs, ci-après l""Utilisateur", du site www.AuBordsDesRues.fr , ci-après le "Site", les présentes
-mentions légales.
-</p>
+    <p>Conformément aux dispositions des articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la
+    Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et
+    visiteurs, ci-après l""Utilisateur", du site www.AuBordsDesRues.fr , ci-après le "Site", les présentes
+    mentions légales.
+    </p>
 
-<br>
+    <br>
 
-<p>La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et sans réserve
-des présentes mentions légales.
-Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».
-</p>
+    <p>La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et sans réserve
+    des présentes mentions légales.
+    Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».
+    </p>
 
-<h3>article 1 - L'EDITEUR</h3>
+    <h3>article 1 - L'EDITEUR</h3>
 
-            <P>L’édition et la direction de la publication du Site est assurée par Nébula Agency (Nehémia Telusma, Ayline TRAVERS, Olivia SIMONET, Auriane-Mirana SICARD-RAZAKA, Lyam Roux), domiciliée 1 rue jack Ellul
-            33000 bordeaux, dont le numéro de téléphone est 07********, et l'adresse e-mail
-            Nébula.agency@gmail.com..
-            ci-après l'"Editeur".
-            </P>
+    <P>L’édition et la direction de la publication du Site est assurée par Nébula Agency (Nehémia Telusma, Ayline TRAVERS, Olivia SIMONET, Auriane-Mirana SICARD-RAZAKA, Lyam Roux), domiciliée 1 rue jack Ellul
+    33000 bordeaux, dont le numéro de téléphone est 07********, et l'adresse e-mail
+    nebulab.agency@gmail.com,
+    ci-après l'"Editeur".
+    </P>
 
-<h3>article 2 - L'HEBERGEUR
-</h3>
+    <h3>article 2 - L'HEBERGEUR</h3>
 
-<p>L'hébergeur du Site est la société AuBordDesRues, dont le siège social est situé au 1 rue jack Ellul
-bordeaux 330000 , avec le numéro de téléphone : 07******** + adresse mail de contact
-</p>
+    <p>L'hébergeur du Site est l'Institut Universitaire de Technologie Bordeaux Montaigne, situé au 1 rue Jacques Ellul
+    bordeaux (330000), dont le numéro de téléphone est 05 57 12 20 44.
+    </p>
 
-<h3>article 3 - ACCES AU SITE
-</h3>
+    <h3>article 3 - ACCES AU SITE</h3>
 
-<p>Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption
-programmée ou non et pouvant découlant d’une nécessité de maintenance.
-En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.</p>
+    <p>Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption
+    programmée ou non et pouvant découlant d’une nécessité de maintenance.
+    En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.</p>
 
-<h3>article 4 - COLLECTE DES DONNEES</h3>
+    <h3>article 4 - COLLECTE DES DONNEES</h3>
 
-<p class="end">Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la
-mesure où il ne collecte aucune donnée concernant les utilisateurs.
-Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site,
-sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires
-telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
-</p>
+    <p class="end">Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la
+    mesure où il ne collecte aucune donnée concernant les utilisateurs.
+
+    Les informations recueillies sont enregistrées dans un fichier informatisé par Nebulab Agency
+    dans le but de mener une activité sur le blog. Elles sont conservées pendant 2 ans maximum et sont destinées à l'administrateur du blog.
+    <br/>
+    Conformément aux lois « Informatique & Liberté » et « RGPD », vous pouvez exercer vos droits d’accès à vos données, de rectification, d’effacement, d’opposition, de limitation ou de portabilité en contactant : 
+    nebulab.agency@gmail.com
+    <br/>
+    En cas de différend, vous avez le droit d’introduire une réclamation (plainte) auprès de la Cnil.
+
+    <p>
+    Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site,
+    sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires
+    telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
+    </p>
 
 
 
